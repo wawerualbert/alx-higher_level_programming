77b944d90e39f24e8097368fecb5a90ec8202cea
wawerualbert <50580926+wawerualbert@users.noreply.github.com>
2022-06-08 00:08:39 +0300

    Create 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+     A function that retrieves an element from a list like in C.
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    return (my_list[idx])
